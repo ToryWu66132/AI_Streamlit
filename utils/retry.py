c86eebf65b232668162ad834a792657ebecb23dd
ToryWu66132 <1434329059@qq.com>
2026-04-21 16:07:07 +0800

    Add files via upload

---
+++ utils/retry.py
@@ -0,0 +1,11 @@
+import time
+
+
+def retry(func, retries=3, delay=2):
+    for i in range(retries):
+        try:
+            return func()
+        except Exception:
+            if i == retries - 1:
+                raise
+            time.sleep(delay)
